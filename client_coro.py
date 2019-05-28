86859e87f1f793d35afb294b36901d9b48bb0eee
Evan DePosit <evandeposit@Evans-MacBook-Pro.local>
2019-05-28 11:10:12 -0700

    changed ports to ints in msgObj.  renamed functions main

--- client_coro.py
+++ client_coro.py
@@ -1,24 +1,39 @@
 #client
 import asyncio
 import sys
+import ast
+
+def new_msgObj(msg):
+    msgObj={}
+    msgObj['contents']= msg
+    return msgObj
+
+def msgObj_to_str(msgObj):
+    return str(msgObj)
+
+def str_to_msgObj(msgStr):
+    return ast.literal_eval(msgStr)
 
 async def send_to_server(writer, loop):
     while True:
         line= await loop.run_in_executor(None, sys.stdin.readline)
-        line=line[::]
         if line:
-            print('Send: %r' % line)
-            writer.write(line.encode())
+            line=line[:-1:]
+            #make msgObj
+            msgObj=new_msgObj(line)
+            msgStr=msgObj_to_str(msgObj)
+            writer.write(msgStr.encode())
             line=None
 
 async def listen_to_server(reader):
     while True:
         data = await reader.read(100)
-        print('Received: %r' % data.decode())
-        await asyncio.sleep(0)
+        if data:
+            print(data.decode())
+            await asyncio.sleep(0)
 
 
-async def tcp_echo_client(loop):
+async def main(loop):
     #opens connection to server
     reader, writer = await asyncio.open_connection('127.0.0.1', 8888, loop=loop)
 
@@ -31,9 +46,12 @@ async def tcp_echo_client(loop):
 
 #problem: this client sends message and then closes loop.  need to listen
 loop = asyncio.get_event_loop()
-loop.run_until_complete(tcp_echo_client(loop))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+loop.run_until_complete(main(loop))
 loop.close()
+
+#loop.run_until_complete(tcp_echo_client(loop))
+#try:
+    #loop.run_forever()
+#except KeyboardInterrupt:
+#    pass
+#loop.close()
--- server_coro.py
+++ server_coro.py
@@ -1,8 +1,50 @@
 import asyncio
+import ast
 
+#addressList is list of all client port numbers(int)
 addressList=[]
+
+#mailbox is dictionary each client port number is key to list of their 
+#undelivered messages
 mailBox={}
-#server
+
+#chatRooms is dictionary. name or number of chatroom is key to list of
+#client ports in chatroom.  server will iterate through a chat room's list
+#and deliver message to each mailbox of each client in list.  Clients will 
+#have to maintain their own list of what rooms they are in on their side.
+ChatRooms={}
+
+def is_connected():
+    #need some way to determine if client is still connected.  if not 
+    #terminate connection and cancel coroutines maybe.  probably a built 
+    #in method for for checking.  Definitly build in method for canceling
+    #coroutines
+    pass
+
+def delete_client(clientPort):
+    #remove client from chatRooms lists and addressList
+    pass
+
+def remove_from_room(clientPort, room):
+    #remove client from specificed chatroom list in chatroom dictionary
+    pass
+
+def add_to_room(clientPort, room):
+    #add client from specificed chatroom list in chatroom dictionary
+    pass
+
+def new_msgObj(msg):
+    #can add client name to msgObj on client side. server adds port number
+    #on its side
+    msgObj={}
+    msgObj['contents']= msg
+    return msgObj
+
+def msgObj_to_str(msgObj):
+    return str(msgObj)
+
+def str_to_msgObj(msgStr):
+    return ast.literal_eval(msgStr)
 
 def new_client(port):
     global addressList
@@ -18,12 +60,14 @@ def new_client(port):
     #set up mailbox
     print('address list: ', addressList)
 
-def broadcast_msg(msg):
+def broadcast_msg(msgObj):
     global addressList
     global mailBox
 
     for port in addressList:
-        mailBox[port].append(msg)
+        if msgObj['from'] != port:
+            msg=str(msgObj['from']) + ': ' + msgObj['contents']
+            mailBox[port].append(msg)
 
 def check_mail(port):
     global addressList
@@ -33,13 +77,17 @@ def check_mail(port):
         msg= mailBox[port].pop(0)
         print('retreiving: ',msg)
         return msg
+    return None
 
 async def listen_to_client(reader, addr):
     while True:
         data = await reader.read(100)
-        message = data.decode()
-        print("Received %r" % (message))
-        broadcast_msg(message)
+        msgStr = data.decode()
+        if msgStr:
+            print("Received %r" % (msgStr))
+            msgObj= str_to_msgObj(msgStr)
+            msgObj['from']=addr[1]
+            broadcast_msg(msgObj)
 
 async def send_to_client(writer, addr):
     while True:
@@ -50,20 +98,20 @@ async def send_to_client(writer, addr):
             msgOut=None
         await asyncio.sleep(0)
 
-async def handle_echo(reader, writer):
+async def main(reader, writer):
     global addressList
 
     addr = writer.get_extra_info('peername')
     new_client(addr[1])
 
-    await asyncio.gather(listen_to_client(reader), send_to_client(writer, addr))
+    await asyncio.gather(listen_to_client(reader, addr), send_to_client(writer, addr))
 
     print("Close the client socket")
     writer.close()
 
 #127.0.0.1 is local host
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+coro = asyncio.start_server(main, '127.0.0.1', 8888, loop=loop)
 server = loop.run_until_complete(coro)
 
 # Serve requests until Ctrl+C is pressed
